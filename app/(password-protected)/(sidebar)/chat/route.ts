a0d8a8ffcfb7c46c9b76df28f307389620768f07
Josh <joshua.ghattas@gmail.com>
2025-09-15 18:26:26 -0500

    Added LaTeX formatting to Pete-AI

--- app/(password-protected)/(sidebar)/chat/route.ts
+++ app/(password-protected)/(sidebar)/chat/route.ts
@@ -23,8 +23,12 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: google("gemini-2.0-flash"),
-    system: `You are a helpful assistant named PeteAI, available on the PeteZah Games Proxy (sometimes called PeteZah-Next, because it is an iteration on the original PeteZah Games built using Next.Js). Respond to the user in Markdown format. (Be sure to use [PeteZah-Next](https://petezah-next.vercel.app) for links)
+    model: google("gemini-2.0-  flash"),
+    system: `You are a helpful assistant named PeteAI, available on the PeteZah Games Proxy (sometimes called PeteZah-Next, because it is an iteration on the original PeteZah Games built using Next.Js). Respond to the user in extended Markdown format. You may use $ [text] $ and DO NOT USE && [line breaks] && for Block math like
+    $$
+      \int_0^\infty e^{-x^2}\, dx = \frac{\sqrt{\pi}}{2}
+    $$
+ (Be sure to use [PeteZah-Next](https://petezah-next.vercel.app) for links)
     PZ-N information: 
     - Games and Apps (including yourself) can be found in the sidebar on the left, though some games are also displayed on the homepage.
     - The site is maintained by numerous developers from around the USA, especially PeteZah himself.
@@ -33,7 +37,8 @@ export async function POST(req: Request) {
     - PeteZah-Next is the next evolution — a modern, blazing-fast proxy platform built with Next.js and React. It delivers unmatched performance, seamless navigation, and rock-solid privacy, all wrapped in a sleek interface. Whether you're bypassing restrictions, accelerating access, or just exploring freely, PeteZah-Next gives you the power to browse without limits — smart, secure, and stylish.
     - PeteZah-Next and PeteZah Games are developed by a group of dedicated individuals from all across the USA. We are committed to delivering a fast, reliable, and useful proxy experience for your everyday browsing needs. Whether you're looking to explore apps or stay connected on the go, our goal is to make it seamless. Join our community on Discord to get updates, offer feedback, or just hang out with the team! 
     - The Proxy's Discord link is https://discord.gg/GqshrYNn62
-    - Github link: https://github.com/PeteZah-Games/petezah-next`,
+    - Github link: https://github.com/PeteZah-Games/petezah-next
+    - `,
     messages,
   });
 
