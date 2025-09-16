3f158bb574634372f0d3a47ca574ee3eb30a9d9d
Josh <joshua.ghattas@gmail.com>
2025-09-16 06:27:59 -0500

    Doing everything I can to fix the proxy

--- public/static/embed.js
+++ public/static/embed.js
@@ -1,25 +1,27 @@
 let destination = "";
 
 try {
-	destination = new URL(location.hash.slice(1));
+  destination = new URL(location.hash.slice(1));
 
-	if (!destination.protocol) {
-		destination = new URL("https://" + destination.href);
-	}
+  if (!destination.protocol) {
+    destination = new URL("https://" + destination.href);
+  }
 } catch (err) {
-	alert(`Bad # string or bad URL. Got error:\n${err}`);
-	alert(destination)
-	console.log(location.hash)
-	throw err;
+  alert(`Bad # string or bad URL. Got error:\n${err}`);
+  alert(destination);
+  console.log(location.hash);
+  throw err;
 }
 
 registerSW()
-	.then(() => {
-		window.open(
-			__uv$config.prefix + __uv$config.encodeUrl(destination.toString()),
-			"_self",
-		);
-	})
-	.catch((err) => {
-		alert(`Encountered error:\n${err}`);
-	});
+  .then(() => {
+    console.log(__uv$config.encodeUrl(destination.toString()));
+    console.log(__uv$config.prefix + destination.toString());
+    window.open(
+      __uv$config.prefix + __uv$config.encodeUrl(destination),
+      "_self"
+    );
+  })
+  .catch((err) => {
+    alert(`Encountered error:\n${err}`);
+  });
